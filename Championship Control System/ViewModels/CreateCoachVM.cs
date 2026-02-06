8553d4bc74b7b3166cb6ec2f160f4556cf2c4bf4
Omar-Reda1 <omar3000om30@gmail.com>
2026-02-06 05:28:57 +0200

    Add Coach CRUD

---
+++ Championship Control System/ViewModels/CreateCoachVM.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace Championship_Control_System.ViewModels
+{
+    public class CreateCoachVM
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        public string? Name { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? BirthDate { get; set; }  
+
+        public int? TeamId { get; set; }
+        
+        public IFormFile ImageFile { get; set; }
+
+        // Dropdown items
+        public IEnumerable<SelectListItem>? Teams { get; set; }
+    }
+}

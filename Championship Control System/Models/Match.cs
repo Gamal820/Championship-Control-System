5b6cffb13a5b55ed1d5933f86a35f65530731751
fathy <fathy@FAT7Y>
2026-02-12 17:29:41 +0200

    Solve the Conflict

--- Championship Control System/Models/Match.cs
+++ Championship Control System/Models/Match.cs
@@ -19,8 +19,6 @@ public partial class Match
 
     public string? Status { get; set; }
 
-    public decimal? TicketPrice { get; set; }
-
     public int? StadiumId { get; set; }
 
     public int? ChampionshipId { get; set; }

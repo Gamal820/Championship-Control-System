f95680f9f052f6b6fbdc44188a905dc53e10d621
Hassan Mokhtar <140200889+hasanmokh@users.noreply.github.com>
2026-02-12 20:20:04 +0200

    Lolization

--- Championship Control System/Areas/Customer/Controllers/HomeController.cs
+++ Championship Control System/Areas/Customer/Controllers/HomeController.cs
@@ -1,13 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Championship_Control_System.Areas.Customer.Controllers
 {
     [Area("Customer")]
     public class HomeController : Controller
     {
+
+     //
+        [HttpPost]
+        public IActionResult SetLanguage(string culture, string returnUrl)
+        {
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+            );
+            return LocalRedirect(returnUrl);
+        }
+
+      
+        //
         public IActionResult Index()
         {
             return View();
         }
+
+
     }
 }
